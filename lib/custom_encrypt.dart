51f3becd424e7660d7f72b1c81c0b8e6bd5309ae
Sneh Mehta <snehmehta933@gmail.com>
2021-10-20 15:47:40 +0530

    encrypt method added

--- lib/custom_encrypt.dart
+++ lib/custom_encrypt.dart
@@ -7,22 +7,23 @@ import 'package:encrypt/encrypt.dart';
 
 class CustomCryptor {
   final String generatedRandomKey;
+  late Encrypter encrypter;
 
-  CustomCryptor({required this.generatedRandomKey});
-
-  String decrypt({
-    required String encryptedValue,
-  }) {
+  CustomCryptor({required this.generatedRandomKey}) {
     final key = Key.fromBase64(generatedRandomKey.split(":")[0]);
 
-    final encrypter = Encrypter(
+    encrypter = Encrypter(
       AES(
         key,
         mode: AESMode.cbc,
         padding: "PKCS7",
       ),
     );
+  }
 
+  String decrypt({
+    required String encryptedValue,
+  }) {
     List<String> cipherTextIvMac = encryptedValue.split(':');
 
     Uint8List bytes = convert.base64.decode(cipherTextIvMac[2]);
@@ -32,5 +33,19 @@ class CustomCryptor {
       iv: IV.fromBase64(cipherTextIvMac[0]),
     );
     return decrypted;
+  }
+
+  String encrypt({
+    required String plainText,
+  }) {
+    final iv = IV.fromLength(16);
+
+    final decrypted = encrypter.encrypt(
+      plainText,
+      iv: iv,
+    );
+    const mac = 'mac';
+    final cipherTextIvMac = '${iv.base64}:$mac:${decrypted.base64}';
+    return cipherTextIvMac;
   }
 }
